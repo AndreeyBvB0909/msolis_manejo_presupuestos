4125f1c852e330f8c435a43df710005aa2833a4e
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-06-21 07:18:47 -0600

    Comienzo proyecto

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,19 @@
+﻿using ManejoPresupuesto.Validaciones;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(maximumLength:50, MinimumLength = 3, ErrorMessage = "La longitud del campo {0} debe estar entre {1}y{2}")]
+        [Display(Name ="Nombre del tipo cuenta")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Order { get; set; }
+
+    }
+}
