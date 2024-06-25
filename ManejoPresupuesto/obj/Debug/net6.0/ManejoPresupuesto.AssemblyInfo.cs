97f08fb3d77130a844141b92a2056223ab666bbb
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-06-25 08:29:54 -0600

    Se agrego una validación para que en tipo cuenta no hayan registros repetidos

--- ManejoPresupuesto/obj/Debug/net6.0/ManejoPresupuesto.AssemblyInfo.cs
+++ ManejoPresupuesto/obj/Debug/net6.0/ManejoPresupuesto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f5c152a100e1518cbd9d5f4c5433ad3f3056ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b5a27782e7303f587fa52adb9d804d1f68fb78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
