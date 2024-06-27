68116c5e7bc534c2fadbb6fa633bb1f7350a4fab
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-06-27 09:15:11 -0600

    Se hicieron algunos ajustes para evitar la repetición de código

--- ManejoPresupuesto/obj/Debug/net6.0/ManejoPresupuesto.AssemblyInfo.cs
+++ ManejoPresupuesto/obj/Debug/net6.0/ManejoPresupuesto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54fcdf86b8916affffedabb54cc88bb799bf7781")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abaacb98e28671c2da23ce8dcee04f3675b6f76e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
