1c5572949f3f96e2519f96494504d13fa04b8919
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-07-04 08:38:57 -0600

    Se persiste el nuevo orden en la base de datos

--- ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -12,6 +12,7 @@ namespace ManejoPresupuesto.Servicios
         Task<bool> Existe(string nombre, int usuarioId);
         Task<IEnumerable<TipoCuenta>> Obtener(int usuarioId);
         Task<TipoCuenta> ObtenerPorId(int id, int usuarioId);
+        Task Ordenar(IEnumerable<TipoCuenta> tipoCuentasOrdenados);
     }
     public class RepositorioTiposCuentas : IRepositorioTiposCuentas
     {
@@ -43,7 +44,8 @@ namespace ManejoPresupuesto.Servicios
             using var connection = new SqlConnection(connectionString);
             return await connection.QueryAsync<TipoCuenta>(@"SELECT Id, Nombre, Orden
                                                             FROM TiposCuentas
-                                                            WHERE UsuarioId = @UsuarioId", new { usuarioId });
+                                                            WHERE UsuarioId = @UsuarioId
+                                                            ORDER BY Orden", new { usuarioId });
         }
 
         public async Task Actualizar(TipoCuenta tipoCuenta)
@@ -67,6 +69,13 @@ namespace ManejoPresupuesto.Servicios
         {
             using var connection = new SqlConnection(connectionString);
             await connection.ExecuteAsync("DELETE TiposCuentas WHERE Id = @Id", new {id});
+        }
+
+        public async Task Ordenar(IEnumerable<TipoCuenta> tipoCuentasOrdenados)
+        {
+            var query = "UPDATE TiposCuentas SET Orden = @Orden WHERE Id = @Id;";
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(query, tipoCuentasOrdenados);
         }
     }
 
