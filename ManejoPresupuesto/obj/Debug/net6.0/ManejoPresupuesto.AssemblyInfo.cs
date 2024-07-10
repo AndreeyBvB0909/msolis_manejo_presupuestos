6347da9879cfe0f073bf7ff650bd3f0cb923aa32
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-07-10 15:41:04 -0600

    Se aisló el css de cuentas

--- ManejoPresupuesto/obj/Debug/net6.0/ManejoPresupuesto.AssemblyInfo.cs
+++ ManejoPresupuesto/obj/Debug/net6.0/ManejoPresupuesto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+442f3cfe7e03dfa2dce3dc9c91a5134ef99f50de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc32092a8e482c37ad565fafe7e5d22b442d42b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManejoPresupuesto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
