0663ccdbe51ee634217dd2d41e4834876395ed06
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-07-15 09:42:53 -0600

    Se hizo el borrar cuenta

--- ManejoPresupuesto/Servicios/RepositorioCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioCuentas.cs
@@ -7,6 +7,7 @@ namespace ManejoPresupuesto.Servicios
     public interface IRepositorioCuentas
     {
         Task Actualizar(CuentaCreacionViewModel cuenta);
+        Task Borrar(int id);
         Task<IEnumerable<Cuenta>> Buscar(int usuarioId);
         Task Crear(Cuenta cuenta);
         Task<Cuenta> ObtenerPorId(int id, int usuarioId);
@@ -54,6 +55,11 @@ namespace ManejoPresupuesto.Servicios
             await connection.ExecuteAsync(@"UPDATE Cuentas
                                             SET Nombre = @Nombre, Balance = @Balance, Descripcion = @Descripcion, TipoCuenta = @TipoCuentaId
                                             WHERE Id = @Id", cuenta);
+        }
+        public async Task Borrar(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync("DELETE Cuentas WHERE Id = @Id", new { id });
         }
     }
 }
